edaa3181f936b372257bf7046890a95f1e6ac38f
Makishimu <makishimu.siroosh@gmail.com>
2017-10-11 00:06:44 +0300

    Add Gallery page template

--- app/js/controllers/gallery.controller.js
+++ app/js/controllers/gallery.controller.js
@@ -2,7 +2,47 @@ function galleryCtrl(){
 
 	var vm = this;
 	
-	console.log('aaa');
+	vm.pictures = [
+		{
+			imgLink : 'img/thumbnails/ArabianJinPre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/ArabianPrincessPre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacter2Pre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacter3Pre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacter4Pre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacter5Pre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacter6Pre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacter7Pre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacter8Pre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/GIcharacterPre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/OrbCharacterPre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/VampireHunterPre.png'
+		},
+		{
+			imgLink : 'img/thumbnails/ZeusPre.png'
+		}
+	]
 };
 
 angular.module('alexGalleryApp')
